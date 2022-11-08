126060e6bf6ce98344fe06ce185e4d089d137f5d
Joaco189 <mangoldjoaquin@gmail.com>
2022-11-08 20:53:02 -0300

    adding ItemCount

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import ItemCount from "../ItemCount/ItemCount"
 import "./ItemDetail.css"
 
 const ItemDetail = ({producto}) => {
     const priceFormat = parseInt(producto.price).toLocaleString("es-ES", {style:"currency", currency:"ARS"})
+
+    const [qty, setQty] = useState(0);
+
+    const purchase = (quantity) => {
+        console.log(quantity);
+        setQty(quantity)
+    }
+
+
     return (
         <div className="item-detail-container">
             <div className="item-detail-card">
@@ -15,6 +26,11 @@ const ItemDetail = ({producto}) => {
                 <div className="item-detail-price">
                     <span>{priceFormat}</span>
                 </div>
+                {qty ? 
+                <button className="go-to-cart">Ir al carrito</button> 
+                :
+                <ItemCount onAdd={purchase} stock={10} initial={1}/>
+                }
             </div>
         </div>
     )
